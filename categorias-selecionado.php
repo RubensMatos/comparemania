d19863bb615bfef9b0f49d18dd37202db645badb
RubensMatos <rubens.matos.filho@gmail.com>
2024-01-22 17:21:05 -0300

    Atualização categorias-selecionado

--- categorias-selecionado.php
+++ categorias-selecionado.php
@@ -1,9 +1,14 @@
 <?php require_once('header.php'); ?>
 
 <div class="container conteudo">
-    <img src="images/banner-categorias-selecionado.svg" class="w-100" />
+    <img src="images/banner-categorias-selecionado.svg" class="w-100 mobile-d-none" />
 
-    <nav aria-label="breadcrumb">
+    <div class="banner-bg mobile-d-block mb-4 bg-secondary bg-gradient">
+        <label class="banner-titulo fonteAzulEscuro">Vantagens exclusivas em nossas lojas parceiras.</label>
+        <label class="banner-texto fonteBranco">Vantagens em pontos, milhas, cashback e descontos, antes de adquirir sua nova TV</label>
+    </div>
+
+    <nav aria-label="breadcrumb" class="mobile-d-none">
         <ol class="breadcrumb pt-4">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item"><a href="#">Categorias</a></li>
@@ -11,8 +16,12 @@
         </ol>
     </nav>
 
-    <div class="col-md-12 text-center mt-4">
+    <div class="col-md-12 text-center mt-4 mobile-d-none">
         <img src="images/anuncio-8.svg" class="zeroAuto anuncio-1-conteudo" />
+    </div>
+
+    <div class="col-md-12 text-center mobile-d-block">
+        <img src="images/anuncio-18.svg" class="zeroAuto anuncio-1-conteudo" />
     </div>
 
     <div class="my-5 w-100 row blocoCategorias-1">
@@ -167,7 +176,8 @@
     </div>
 
     <div class="col-md-12 text-center mt-0 mb-5">
-        <img src="images/anuncio-8.svg" class="zeroAuto anuncio-1-conteudo" />
+    <img src="images/anuncio-8.svg" class="zeroAuto anuncio-1-conteudo mobile-d-none" />
+    <img src="images/anuncio-18.svg" class="zeroAuto anuncio-1-conteudo mobile-d-block" />
     </div>
 
 </div>
