1c0109e7c741b4ba7272b18ed251e6a808a389a5
RubensMatos <rubens.matos.filho@gmail.com>
2024-01-22 15:43:05 -0300

    Atualização termos-de-uso

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Layout Comparemania
 # Validação mobile / tablet / desktop
 <p>https://dev.solutioweb.com.br/comparemania/sobre-nos.php</p>
 <p>https://dev.solutioweb.com.br/comparemania/politica-de-privacidade.php</p>
+<p>https://dev.solutioweb.com.br/comparemania/termos-de-uso.php</p>
--- termos-de-uso.php
+++ termos-de-uso.php
@@ -1,17 +1,27 @@
 <?php require_once('header.php'); ?>
 
 <div class="container conteudo">
-    <img src="images/banner-termos.svg" class="w-100" />
 
-    <nav aria-label="breadcrumb">
+    <img src="images/banner-termos.svg" class="w-100 mobile-d-none" />
+
+    <div class="banner-bg mobile-d-block mb-4">
+        <label class="banner-titulo pt-4">Termos de Uso</label>
+        <label class="banner-texto">Entenda nossas Diretrizes.</label>
+    </div>
+
+    <nav aria-label="breadcrumb" class="mobile-d-none">
         <ol class="breadcrumb pt-4">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Termos de Uso</li>
         </ol>
     </nav>
 
-    <div class="col-md-12 text-center mt-4">
+    <div class="col-md-12 text-center mt-4 mobile-d-none">
         <img src="images/anuncio-8.svg" class="zeroAuto anuncio-1-conteudo" />
+    </div>
+
+    <div class="col-md-12 text-center mobile-d-block">
+        <img src="images/anuncio-18.svg" class="zeroAuto anuncio-1-conteudo" />
     </div>
 
     <div class="w-100">
@@ -28,26 +38,38 @@
                 <br />&#x25CF; Este site não usa cookies para armazenar informações em seu computador local.
                 <br />&#x25CF; Todas as imagens exibidas, exceto o logotipo Comparemania, pertencem ao site de origem e
                 são exibidas apenas para referência.
-            
+
             </span>
         </div>
     </div>
 
-<div class="w-100">
-    <div class="card card-1 border-0 b-r-20 p-4 h-auto mt-4 mb-3">
-        <span class="NunitoGrey500-2 w-100 left text-start pt-2">O site Comparemania não requer qualquer tipo de login, registro ou informação pessoal. Todo trabalho é feito respeitando a privacidade e intimidade de nossos usuários. Todos os dados e métricas do usuário são coletados anonimamente pelo Google Analytics e seguem sua política de privacidade.</span>
+    <div class="w-100">
+        <div class="card card-1 border-0 b-r-20 p-4 h-auto mt-4 mb-3">
+            <span class="NunitoGrey500-2 w-100 left text-start pt-2">O site Comparemania não requer qualquer tipo de
+                login, registro ou informação pessoal. Todo trabalho é feito respeitando a privacidade e intimidade de
+                nossos usuários. Todos os dados e métricas do usuário são coletados anonimamente pelo Google Analytics e
+                seguem sua política de privacidade.</span>
+        </div>
     </div>
-</div>
 
-<div class="w-100">
-    <div class="card card-1 border-0 b-r-20 p-4 h-auto mt-4 mb-3">
-        <span class="NunitoGrey500-2 w-100 left text-start pt-2">O Comparemania é um site de redirecionamento que sempre exibe a bonificação máxima disponível em cada promoção em nossa plataforma. No entanto, é importante destacar que cada promoção, loja ou programa tem suas próprias condições e regulamentos específicos. Para evitar possíveis problemas, é altamente recomendável que você verifique cuidadosamente o regulamento antes de realizar qualquer operação ou transação.</span>
+    <div class="w-100">
+        <div class="card card-1 border-0 b-r-20 p-4 h-auto mt-4 mb-3">
+            <span class="NunitoGrey500-2 w-100 left text-start pt-2">O Comparemania é um site de redirecionamento que
+                sempre exibe a bonificação máxima disponível em cada promoção em nossa plataforma. No entanto, é
+                importante destacar que cada promoção, loja ou programa tem suas próprias condições e regulamentos
+                específicos. Para evitar possíveis problemas, é altamente recomendável que você verifique cuidadosamente
+                o regulamento antes de realizar qualquer operação ou transação.</span>
+        </div>
     </div>
-</div>
 
     <div class="w-100 pt-4 pb-5">
-        <span class="w-60-zeroAuto NunitoBlue700-2 text-start bgDark-fonte-1">Em caso de dúvidas sobre esta Política de Privacidade, não hesite
+        <span class="w-60-zeroAuto NunitoBlue700-2 text-start bgDark-fonte-1 w-100-mob">Em caso de dúvidas sobre esta
+            Política de Privacidade, não hesite
             em nos <a href="#" class="NunitoBlue700-2-link bgDark-fonte-1">Contatar</a></span>
+    </div>
+
+    <div class="col-md-12 text-center mb-4 mobile-d-block">
+        <img src="images/anuncio-17.svg" class="zeroAuto anuncio-1-conteudo" />
     </div>
 
 </div>
