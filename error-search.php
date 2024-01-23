229e0e15f721cb11db49257a6bd50129f01c56f5
RubensMatos <rubens.matos.filho@gmail.com>
2024-01-23 11:14:02 -0300

    Atualização error-search

--- error-search.php
+++ error-search.php
@@ -5,6 +5,12 @@
     <div class="my-5 w-100 d-flex conteudo">
 
         <div class="col-md-6 ps-5">
+
+            <div class="w-100 text-center pb-5 mobile-d-block">
+                <img src="images/logo-2.svg" class="pt-0 img404 zeroAuto" />
+                <img src="images/logo-2-dark.svg" class="pt-0 img404-dark zeroAuto" />
+            </div>
+
             <span class="NunitoBlue500-4 bgDark-fonte-1">
                 Ops...
                 <br />Não conseguimos encontrar a página que você procura.
@@ -12,12 +18,12 @@
             </span>
 
             <div class="w-100 mt-5 text-center">
-                <a class="botaoRedondo1 my-3 left w-100 text-center text-decoration-none cursror pointer w-410px zeroAuto" href="#"><img
-                        src="images/arrow-right-1.svg" class="transform180"> &nbsp; Voltar</a>
+                <a class="botaoRedondo1 my-3 left w-100 text-center text-decoration-none cursror pointer w-410px zeroAuto"
+                    href="#"><img src="images/arrow-right-1.svg" class="transform180"> &nbsp; Voltar</a>
             </div>
         </div>
 
-        <div class="col-md-6 text-center pt-5 mt-5">
+        <div class="col-md-6 text-center pt-5 mt-5 mobile-d-none">
             <img src="images/logo-2.svg" class="pt-5 img404 zeroAuto" />
             <img src="images/logo-2-dark.svg" class="pt-5 img404-dark zeroAuto" />
         </div>
