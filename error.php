92a2608bcd30c4c27803c25c1cfc38ac38cc6658
RubensMatos <rubens.matos.filho@gmail.com>
2024-01-23 11:07:13 -0300

    Atualização error

--- error.php
+++ error.php
@@ -11,13 +11,16 @@
 
     <div class="my-5 w-100 d-flex conteudo pt-5">
 
-        <div class="col-md-6 ps-5">
+        <div class="col-md-6 ps-5 px-4-mob">
+
+            <div class="w-100 text-center pb-5 mobile-d-block">
+                <img src="images/logo-2.svg" class="pt-0 img404 zeroAuto" />
+                <img src="images/logo-2-dark.svg" class="pt-0 img404-dark zeroAuto" />
+            </div>
+
             <span class="NunitoBlue500-4 bgDark-fonte-1 mt-3">
                 Ops...
-                <br />Estamos passando por um 
-                <br />problema de instabilidade.
-                <br />Por favor, tente novamente 
-                mais tarde!
+                <br />Estamos passando por um problema de instabilidade. Por favor, tente novamente mais tarde!
             </span>
 
             <div class="w-100 mt-5 text-center">
@@ -26,7 +29,7 @@
             </div>
         </div>
 
-        <div class="col-md-6 text-center pt-5 mt-3">
+        <div class="col-md-6 text-center pt-5 mt-3 mobile-d-none">
             <img src="images/logo-2.svg" class="pt-0 img404 zeroAuto" />
             <img src="images/logo-2-dark.svg" class="pt-0 img404-dark zeroAuto" />
         </div>
